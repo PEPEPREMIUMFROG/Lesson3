f39ac6ee5da1d4e37a5b3d09397f81bd8a42f781
Alexander <TheThirteenthLight@yandex.ru>
2025-07-05 14:23:13 +0300

    feat: added constant for number of employees

--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -5,11 +5,13 @@ import net.datafaker.Faker;
 import java.util.Random;
 
 public class Main {
+    private static final int NUMBER_OF_EMPLOYEES = 5;
+
     public static void main(String[] args) {
         Random random = new Random();
         Faker faker = new Faker();
 
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[NUMBER_OF_EMPLOYEES];
         for (int i = 0; i <= employees.length-1; i++) {
             employees[i] = new Employee(faker.name().firstName(), faker.name().lastName(), faker.name().firstName(),
                     faker.job().position(), faker.internet().emailAddress(), faker.phoneNumber().phoneNumber(),
