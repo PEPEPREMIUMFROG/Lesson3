c0fdb11f929c9d1ef908927ce22bbd672e7eb246
Alexander <TheThirteenthLight@yandex.ru>
2025-07-05 14:20:02 +0300

    feat: added tasks from lesson9 homework

---
+++ src/Lesson9/Main.java
@@ -0,0 +1,26 @@
+package Lesson9;
+
+import net.datafaker.Faker;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Faker faker = new Faker();
+
+        Employee[] employees = new Employee[5];
+        for (int i = 0; i <= employees.length-1; i++) {
+            employees[i] = new Employee(faker.name().firstName(), faker.name().lastName(), faker.name().firstName(),
+                    faker.job().position(), faker.internet().emailAddress(), faker.phoneNumber().phoneNumber(),
+                    random.nextInt(18, 70), random.nextInt(75000, 100000));
+        }
+
+        for (Employee employee : employees) {
+            if (employee.getAge() > 40) {
+                employee.printInfo(System.out);
+            }
+        }
+    }
+
+}
