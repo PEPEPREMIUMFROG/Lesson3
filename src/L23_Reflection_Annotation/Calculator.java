6a01904fd714a7ee448d22919ff87ab6530ab290
Alexander <TheThirteenthLight@yandex.ru>
2025-09-14 16:09:07 +0300

    feat: added classes for lesson 22 homework

---
+++ src/L23_Reflection_Annotation/Calculator.java
@@ -0,0 +1,14 @@
+package L23_Reflection_Annotation;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int subtract(int a, int b){
+        return a-b;
+    }
+    public int multiply(int a, int b){
+        return a*b;
+    }
+}
---
+++ src/L23_Reflection_Annotation/Main.java
@@ -0,0 +1,7 @@
+package L23_Reflection_Annotation;
+
+public class Main {
+    public static void main(String[] args) {
+    TestRunner.start(CalculatorTest.class);
+    }
+}
